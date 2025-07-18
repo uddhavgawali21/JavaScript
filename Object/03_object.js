f2be2d1a03e47ae23816f8a17b53d7d8ea29ad80
Uddhav Gawali <uddhavgawali021@gmail.com>
2025-07-18 20:48:52 +0530

    Object in JS

---
+++ Object/03_object.js
@@ -0,0 +1,8 @@
+const course ={
+    courseName :"JS With chai",
+    courseFee: 999,
+    courseInstructor: "Hitesh"
+}
+
+const {courseInstructor : Teacher} =course       //Object destructuring 
+console.log(Teacher);
