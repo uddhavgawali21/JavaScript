d2a793cbaccbf74f03d5d0f4b5aa3842c1aea27b
uddhavgawali21 <146969862+uddhavgawali21@users.noreply.github.com>
2025-07-29 14:49:18 +0530

    Delete Object directory

--- Object/03_object.js
+++
@@ -1,8 +0,0 @@
-const course ={
-    courseName :"JS With chai",
-    courseFee: 999,
-    courseInstructor: "Hitesh"
-}
-
-const {courseInstructor : Teacher} =course       //Object destructuring 
-console.log(Teacher);
